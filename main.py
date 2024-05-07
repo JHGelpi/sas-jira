3bc017a50c5f7a7e3f58e43c9c7fa9ab1aedefaa
JHGelpi <wes.gelpi@icloud.com>
2024-05-07 11:24:50 -0400

    venv setup and main init

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from jira import JIRA
+
+# Your server URL 
+options = {'server': 'https://rndjira.sas.com/projects/COMPUTESVCS/'}
+
+# Connect to JIRA
+file_path = '/Users/wegelpi/encrypt/secrets/jira-token.txt'
+
+with open(file_path, 'r') as file:
+    jira_api_token = file.read().strip()
+
+jira_email = 'wes.gelpi@sas.com'
+
+try:
+    jira = JIRA(options, basic_auth=(jira_email, jira_api_token))
+    print("Authentication successful")
+except Exception as e:
+    print(f"Failed to authenticate: {e}")
+    if hasattr(e, 'response'):
+        print("Status code:", e.response.status_code)
+        print("Error response:", e.response.text)
+# jira = JIRA(options, basic_auth=(jira_email, jira_api_token))
+
+# Example of fetching an issue
+# issue = jira.issue('COMPUTESVCS-61066')
+# print(issue.fields.summary)  # Prints the summary of the issue
 
