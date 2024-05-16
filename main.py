0675fa444a7509428eda31b8829a443f2d40729c
JHGelpi <wes.gelpi@icloud.com>
2024-05-16 12:36:17 -0400

    added date element

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ jira._session.headers.update({'Authorization': f'Bearer {jira_api_token}'})
 
 # Retrieve all open issues from the COMPUTESVCS and GEMINI project
 #jql_query = build_jql_active()
-jql_query = build_jql_completed('2024.05')
+jql_query = build_jql_completed('2024.04')
 
 # Initialize pagination
 start_at = 0
@@ -54,7 +54,7 @@ csv_file = f'/Users/wegelpi/jira/jira-output-{timestamp}.csv'
 with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
     #writer = csv.writer(file)
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)  # Ensure all fields are quoted
-    writer.writerow(['Epic Link', 'Parent Link','Operational Epic Team', 'Operational Flag', 'Triage Origin', 'Pipeline Discovery Stage','Bug Origin', 'Escaped Bug', 'Fix Version', 'Component','Issue Key', 'Summary', 'Issue URL', 'Type', 'State', 'Assignee', 'Status', 'Start Date', 'End Date', 'Sprint Name', 'Labels', 'Sprint Owner'])
+    writer.writerow(['Epic Link', 'Parent Link','Operational Epic Team', 'Operational Flag', 'Triage Origin', 'Pipeline Discovery Stage','Bug Origin', 'Escaped Bug', 'Fix Version', 'Component','Issue Key', 'Summary', 'Issue URL', 'Type', 'State', 'Assignee', 'Status', 'Start Date', 'End Date', 'Sprint Name', 'Labels', 'Sprint Owner', 'Export Date'])
 
     for issue in all_issues:
         # Extract the fields you need from each issue
@@ -77,12 +77,13 @@ with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
         triage_flg = 'Y' if triage_parser(labels) else 'N'
         oper_flg = 'Y' if oper_parser(labels) else 'N'
         fix_version = parse_fix_version_data(getattr(issue.fields, 'fixVersions', ''))
+        export_date = start_date.strftime('%Y-%m-%d %H:%M:%S')
         #fix_version = getattr(issue.fields, 'fixVersions', '')
         #components = getattr(issue.fields, 'components', '')
         #components_string = getattr(issue.fields, 'components', '')
         components = parse_component_data(getattr(issue.fields, 'components', ''))
 
-        writer.writerow([epic_link, parent_link,oper_epic, oper_flg, triage_flg, pipeline_stage, bug_origin, escaped_bug, fix_version, components, issue.key, issue.fields.summary, issue_url, type, parsed_sprint_data[0], assignee, status, sprint_start_date, sprint_end_date, parsed_sprint_data[3], labels, parsed_sprint_data[4]])
+        writer.writerow([epic_link, parent_link,oper_epic, oper_flg, triage_flg, pipeline_stage, bug_origin, escaped_bug, fix_version, components, issue.key, issue.fields.summary, issue_url, type, parsed_sprint_data[0], assignee, status, sprint_start_date, sprint_end_date, parsed_sprint_data[3], labels, parsed_sprint_data[4], export_date])
 
 append_csv(csv_file)
 start_date = datetime.now()
